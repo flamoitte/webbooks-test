03153ffdfe4ee3f09d357bf80266010060a206f8
flamoitte <frederic.lamoitte@oecd.org>
2023-02-11 08:53:52 +0100

    new nextjs webbook build

---
+++ eag/_next/static/1jOjSbtG2OQwkY32or5W6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-02cc11fd74b4e5ff.js"],"/[...paths]":["static/chunks/pages/[...paths]-998925860f569b4a.js"],sortedPages:["/_app","/_error","/[...paths]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
