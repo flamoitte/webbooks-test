e20b75eca522ca1e8c40894fcfce1600eb949c13
flamoitte <frederic.lamoitte@oecd.org>
2023-06-07 22:25:00 +0200

    Second version GAAG

---
+++ gaag/_next/static/VD0O54herPQ7sjPSpEohR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-02cc11fd74b4e5ff.js"],"/[...paths]":["static/chunks/pages/[...paths]-0c4ac6e70a826379.js"],sortedPages:["/_app","/_error","/[...paths]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
