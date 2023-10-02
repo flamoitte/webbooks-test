c94f26c93b6b64dc45e48462610cfb53320f3a82
flamoitte <frederic.lamoitte@oecd.org>
2023-10-02 17:59:52 +0200

    add climate action monitor 2023

---
+++ climate-action-report/_next/static/5QkqzdHScQoAvIMxRGzgl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-02cc11fd74b4e5ff.js"],"/[...paths]":["static/chunks/pages/[...paths]-5c626cdacbebeecc.js"],sortedPages:["/_app","/_error","/[...paths]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
