22f8fca1cc11195d32ac47539204cfe8dd1d4c0b
Frédéric LAMOITTE <frederic.lamoitte@oecd.org>
2024-05-02 08:44:57 +0200

    Webbook EAG European Union

---
+++ eag-euep/_next/static/KisEz2DyRkkwmrfJK-T8T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9222faf9ee4a01b4.js"],"/[...paths]":["static/chunks/pages/[...paths]-885f6e56f18033c2.js"],sortedPages:["/_app","/_error","/[...paths]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
